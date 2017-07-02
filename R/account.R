549ce1102ce8e360a32d73662e53af5c60938c20
Thomas J. Leeper <thosjleeper@gmail.com>
2017-07-02 07:18:46 +0000

    version 0.1.6

---
+++ R/account.R
@@ -0,0 +1,13 @@
+#' @title AWS Lambda Account Settings
+#' @description Get account settings
+#' @template dots
+#' @return A list.
+#' @examples
+#' \dontrun{
+#'   get_lambda_account()
+#' }
+#' @export
+get_lambda_account <- function(...) {
+    act <- paste0("/2016-08-19/account-settings")
+    lambdaHTTP(verb = "GET", action = act, ...)
+}
